99ed430863a1eb0c608861602a038ee14228bd80
Benjamin Soh <benjamin12357@gmail.com>
2025-09-20 10:40:16 +0800

    added output parsing

--- main.py
+++ main.py
@@ -1,25 +1,44 @@
+from ast import parse
 from dotenv import load_dotenv
+from langchain import hub
+from langchain.agents import AgentExecutor, create_react_agent
+from langchain_core.output_parsers import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 from langchain_ollama import ChatOllama
 from langchain_openai import ChatOpenAI
-from langchain import hub
-from langchain.agents import AgentExecutor
-from langchain.agents import create_react_agent
 from langchain_tavily import TavilySearch
+
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
 
 load_dotenv()
 
 tools = [TavilySearch()]
 llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 react_prompt = hub.pull("hwchase17/react")
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tool_names"]
+).partial(format_instructions=output_parser.get_format_instructions())
 
-agent = create_react_agent(llm, tools, prompt=react_prompt)
+
+agent = create_react_agent(llm, tools, prompt=react_prompt_with_format_instructions)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-chain = agent_executor
+extract_output = RunnableLambda(lambda x: x["output"])
+parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+chain = agent_executor | extract_output | parse_output
+
 
 def main():
-    result = chain.invoke({"input": "search for the latest news on Electric Vehicles and any conferences or symposiums related to EVs"})
+    result = chain.invoke(
+        {
+            "input": "search for the latest news on Electric Vehicles and any conferences or symposiums related to EVs"
+        }
+    )
     print(result)
+
 
 if __name__ == "__main__":
     main()
